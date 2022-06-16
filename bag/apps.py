6fac5e664f485afe22038417f1af1fba6b6ac9d9
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-06-16 15:45:34 +0000

    added forms, admin and signals

--- bag/apps.py
+++ bag/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class BagConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bag'
+class CheckoutConfig(AppConfig):
+    name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
