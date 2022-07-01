528cb571212b05ca83a9d68877f8c40f4ae0a575
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-07-01 12:40:16 +0000

    fixed avrage rating calculation so its working and  added socials

--- products/models.py
+++ products/models.py
@@ -45,13 +45,11 @@ class Product(models.Model):
 
 class Review(models.Model):
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
-    rating = models.IntegerField(default=3)
     content = models.TextField()
     created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    RATINGS = (('1 ',1),('2 ',2),
-          ('3 ',3),('4 ',4),('5 ',5))
-    rating = models.CharField(max_length=7,choices= RATINGS)
+    RATINGS = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+    rating = models.IntegerField(default=RATINGS[2], choices=RATINGS)
 
     def __str__(self):
         return '%s - %s' % (self.product.name, self.created_by)
