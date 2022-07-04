c426cf0ae2dd079a3d7fa543ed83cb9973470429
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-07-04 17:23:35 +0000

    added bootstrap/css to the overal styling of the page

--- eshop/settings.py
+++ eshop/settings.py
@@ -172,12 +172,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Stripe
-FREE_DELIVERY_THRESHOLD = 50
+FREE_DELIVERY_THRESHOLD = 75
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+DEFAULT_FROM_EMAIL = 'hardwareguru@example.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
