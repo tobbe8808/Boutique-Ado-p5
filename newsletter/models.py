b4456af751c07a97d28ddc9c903b05ae059b7074
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-07-07 11:21:06 +0000

    added so you cant enter same email on newletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Subscriber(models.Model):
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return '%s' % self.email
+
+
+
